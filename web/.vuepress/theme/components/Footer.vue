05bb99cd906176c36e82be3c46238b228dedfcc4
ouyangjiahong <ouyangjiahong22@nudt.edu.cn>
2026-03-05 12:49:52 +0800

    feat: 移除微信分享功能及相关样式
    - 删除Footer组件中的微信分享按钮及相关HTML结构
    - 移除对应的JavaScript方法：isWechatBrowser、copyCurrentLink、shareToWechatLink
    - 清理相关的CSS样式，包括.mobile-share、.share-link-btn等类
    - 删除移动端响应式显示规则
    
    简化了页脚组件，移除了不再需要的微信分享功能，使代码更加简洁。

--- web/.vuepress/theme/components/Footer.vue
+++ web/.vuepress/theme/components/Footer.vue
@@ -6,11 +6,6 @@
                 <span class="item-text">{{ item.label }}</span>
             </a>
         </div>
-    <div class="footer-item mobile-share">
-      <button type="button" class="share-link-btn" @click="shareToWechatLink">
-        微信分享链接
-      </button>
-    </div>
         <div class="copy-right">
            <span class="name">{{`${currentYear} 地月空间入门指南 &nbsp; |  &nbsp; `}} </span>
             <a :href="government.href" target="_blank" rel="noreferrer" >
@@ -36,57 +31,6 @@ export default {
     this.footerList = this.$site.themeConfig.footer.friendLinks
     this.government = this.$site.themeConfig.footer.copyright
     this.currentYear =  new Date().getFullYear()
-  },
-
-  methods: {
-    isWechatBrowser () {
-      return /micromessenger/i.test(window.navigator.userAgent)
-    },
-
-    async copyCurrentLink () {
-      const url = window.location.href
-      if (window.navigator.clipboard && window.isSecureContext) {
-        await window.navigator.clipboard.writeText(url)
-        return
-      }
-
-      const textarea = document.createElement('textarea')
-      textarea.value = url
-      textarea.setAttribute('readonly', '')
-      textarea.style.position = 'absolute'
-      textarea.style.left = '-9999px'
-      document.body.appendChild(textarea)
-      textarea.select()
-      document.execCommand('copy')
-      document.body.removeChild(textarea)
-    },
-
-    async shareToWechatLink () {
-      const shareData = {
-        title: document.title,
-        text: document.title,
-        url: window.location.href,
-      }
-
-      // In WeChat browser, users must use the top-right menu to share.
-      if (this.isWechatBrowser()) {
-        await this.copyCurrentLink()
-        window.alert('已复制当前链接。请点击右上角“...”并选择“发送给朋友”进行微信分享。')
-        return
-      }
-
-      if (window.navigator.share) {
-        try {
-          await window.navigator.share(shareData)
-          return
-        } catch (error) {
-          // Ignore user-cancel and fall back to copy link.
-        }
-      }
-
-      await this.copyCurrentLink()
-      window.alert('已复制当前链接，请粘贴到微信发送给好友。')
-    }
   }
 }
 </script>
@@ -109,18 +53,6 @@ export default {
     justify-content center
     align-items center
     color #85858a
-.mobile-share
-  display none
-.share-link-btn
-  border 1px solid #c7ccd1
-  background-color #fff
-  color #2f3a45
-  border-radius 6px
-  padding 0.3rem 0.8rem
-  cursor pointer
-  font-size 0.9rem
-.share-link-btn:hover
-  background-color #f7f9fb
 
 .item-icon
   width 1.4rem
@@ -134,8 +66,4 @@ export default {
     color #85858a
 .copy-right .name
     margin-right 0.4rem
-
-@media (max-width: $MQMobile)
-  .mobile-share
-    display block
 </style>
