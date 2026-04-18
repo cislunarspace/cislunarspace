590cef1542b6952a0d693c942000d349906844dd
ouyangjiahong <ouyangjiahong@github.com>
2026-04-18 11:26:31 +0800

    chore: remove legacy VuePress 1 theme and components

--- web/.vuepress/theme/index.js
+++
@@ -1,9 +0,0 @@
-// .vuepress/theme/index.js
-const path = require('path')
-
-module.exports = {
-    extend: '@vuepress/theme-default',
-    enhanceAppFiles: [
-        path.resolve(__dirname, 'enhanceApp.js')
-    ]
-}
